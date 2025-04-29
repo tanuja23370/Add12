2534c8fd594d1062c3e3580d87b8c6cc7a4dc3e3
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-04-29 12:01:38 -0300

    save

---
+++ Add.java
@@ -0,0 +1,10 @@
+package Add12;
+
+public class Add {
+    public static void main(String args[]) {
+        int a = 12;
+        int b = 27;
+        System.out.println("Addition " + (a + b));  
+        System.out.println("Subtraction " + (a - b));
+    }
+}
