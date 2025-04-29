072acb733c7fba2036fd452f4a0eae267e154a2f
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-04-29 12:06:36 -0300

    save

--- Add.java
+++ Add.java
@@ -6,5 +6,6 @@ public class Add {
         int b = 27;
         System.out.println("Addition " + (a + b));  
         System.out.println("Subtraction " + (a - b));
+        System.out.println("Multiplication " + (a * b));
     }
 }
